1bed536f3812d855aa1d598dc87c1d6914d000e4
Jordan Dimov <jdimov@a115.co.uk>
2023-03-29 14:37:13 +0100

    Formats with black

--- src/coresnap.py
+++ src/coresnap.py
@@ -3,21 +3,23 @@ import typer
 
 
 def sanitize_title(title):
-    title_sanitized = re.sub(r'[^\w\s]', '_', title).replace(' ', '_')
+    title_sanitized = re.sub(r"[^\w\s]", "_", title).replace(" ", "_")
     return title_sanitized
 
+
 def create_node(title):
-    title_clean = title.lstrip().lstrip('-').strip()
+    title_clean = title.lstrip().lstrip("-").strip()
     title_sanitized = sanitize_title(title_clean)
     return (title_clean, title_sanitized)
 
+
 def outline_to_dot(outline):
-    lines = outline.strip().split('\n')
-    dot_lines = ['digraph G {', 'rankdir=TB;', 'node [shape=box];', 'splines=ortho;']
+    lines = outline.strip().split("\n")
+    dot_lines = ["digraph G {", "rankdir=TB;", "node [shape=box];", "splines=ortho;"]
 
     stack = []
     for line in lines:
-        indent = len(re.match(r'\s*', line).group(0))
+        indent = len(re.match(r"\s*", line).group(0))
         title_clean, title_sanitized = create_node(line)
 
         dot_lines.append(f'{title_sanitized} [label="{title_clean}"];')
@@ -29,19 +31,28 @@ def outline_to_dot(outline):
             parent = stack[-1]
             dot_lines.append(f'{parent["title_sanitized"]} -> {title_sanitized}')
 
-        stack.append({"title": title_clean, "title_sanitized": title_sanitized, "level": indent})
+        stack.append(
+            {"title": title_clean, "title_sanitized": title_sanitized, "level": indent}
+        )
 
-    dot_lines.append('}')
-    return '\n'.join(dot_lines)
+    dot_lines.append("}")
+    return "\n".join(dot_lines)
 
 
 def outline_to_dot2(outline):
-    lines = outline.strip().split('\n')
-    dot_lines = ['digraph G {', 'rankdir=LR;', 'node [shape=box];', 'splines=ortho;', 'ranksep=1.0;', 'nodesep=0.4;']
+    lines = outline.strip().split("\n")
+    dot_lines = [
+        "digraph G {",
+        "rankdir=LR;",
+        "node [shape=box];",
+        "splines=ortho;",
+        "ranksep=1.0;",
+        "nodesep=0.4;",
+    ]
 
     stack = []
     for line in lines:
-        indent = len(re.match(r'\s*', line).group(0))
+        indent = len(re.match(r"\s*", line).group(0))
         title_clean, title_sanitized = create_node(line)
 
         dot_lines.append(f'{title_sanitized} [label="{title_clean}"];')
@@ -53,10 +64,12 @@ def outline_to_dot2(outline):
             parent = stack[-1]
             dot_lines.append(f'{parent["title_sanitized"]} -> {title_sanitized}')
 
-        stack.append({"title": title_clean, "title_sanitized": title_sanitized, "level": indent})
+        stack.append(
+            {"title": title_clean, "title_sanitized": title_sanitized, "level": indent}
+        )
 
-    dot_lines.append('}')
-    return '\n'.join(dot_lines)
+    dot_lines.append("}")
+    return "\n".join(dot_lines)
 
 
 def main(input_file: str, output_file: str = "output.dot"):
@@ -65,7 +78,7 @@ def main(input_file: str, output_file: str = "output.dot"):
 
     dot_content = outline_to_dot2(outline)
 
-    input_stem = input_file.rpartition('.')[0]
+    input_stem = input_file.rpartition(".")[0]
 
     with open(output_file, "w") as f:
         f.write(dot_content)
